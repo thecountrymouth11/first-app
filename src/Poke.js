5426476735469eb7706d9f5748d9772b079cc26d
Tarundeshmukh <tarundeshmukh1997@gmail.com>
2023-03-28 16:33:58 +0530

    adding data from file

---
+++ src/Poke.js
@@ -0,0 +1,75 @@
+// array of objects or data
+export const Poke = [
+  {
+    name: "Squirtle",
+    type: "Water",
+    image: "./images/pokemon1.jpg",
+    id: 1,
+  },
+  {
+    name: "Bulbasaur",
+    type: "Grass/Poison",
+    image: "./images/pokemon2.jpg",
+    id: 2,
+  },
+  {
+    name: "Snorlax",
+    type: "Normal",
+    image: "./images/pokemon3.jpg",
+    id: 3,
+  },
+  {
+    name: "Pickachu",
+    type: "Electric",
+    image: "./images/pokemon4.jpeg",
+    id: 4,
+  },
+  {
+    name: "Evee",
+    type: "Normal",
+    image: "./images/pokemon5.jpg",
+    id: 5,
+  },
+  {
+    name: "Blastoise",
+    type: "Water",
+    image: "./images/pokemon6.jpg",
+    id: 6,
+  },
+  {
+    name: "Charmendar",
+    type: "Fire",
+    image: "./images/pokemon7.jpg",
+    id: 7,
+  },
+  {
+    name: "Gangar",
+    type: "Ghost",
+    image: "./images/pokemon8.jpg",
+    id: 8,
+  },
+  {
+    name: "ButterFree",
+    type: "Bug",
+    image: "./images/pokemon9.jpg",
+    id: 9,
+  },
+  {
+    name: "Pidgey",
+    type: "Flying",
+    image: "./images/pokemon10.jpg",
+    id: 10,
+  },
+  {
+    name: "Jigglypuff",
+    type: "Fairy",
+    image: "./images/pokemon11.jpeg",
+    id: 11,
+  },
+  {
+    name: "Geodude",
+    type: "Rock",
+    image: "./images/pokemon12.jpeg",
+    id: 12,
+  },
+];
